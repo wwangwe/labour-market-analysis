58708674a6a11cbe18ab789e0d647e2b00b3ba8a
wwangwe <wangwetymothy@gmail.com>
2022-03-15 00:48:50 +0300

    Setup Database System.

---
+++ system/system/urls.py
@@ -0,0 +1,8 @@
+"""system URL Configuration"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+]
