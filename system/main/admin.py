6b52fb01c9396a0d064de1f14ec55a85f90a4079
wwangwe <wangwetymothy@gmail.com>
2022-03-16 17:11:00 +0300

    Register Database Model(s)

--- system/main/admin.py
+++ system/main/admin.py
@@ -5,5 +5,5 @@ from main import models
 
 @admin.register(models.Job)
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('date', 'title', 'location', 'function', 'industry')
-    list_filter = ('date', 'location', 'function', 'industry')
+    list_display = ('scrap_date', 'title', 'location', 'function', 'industry')
+    list_filter = ('scrap_date', 'location', 'function', 'industry')
