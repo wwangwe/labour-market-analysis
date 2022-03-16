7aa9743598027b39e53f51f40939bbb3706fdce4
wwangwe <wangwetymothy@gmail.com>
2022-03-16 17:10:11 +0300

    Setup Database Schema

--- system/main/models.py
+++ system/main/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 class Job(models.Model):
-    date = models.DateTimeField()
-    title = models.CharField(max_length=75, blank=False)
-    location = models.CharField(max_length=20, blank=False)
-    function = models.CharField(max_length=50, blank=False)
-    industry = models.CharField(max_length=50, blank=False)
-    description = models.TextField(max_length=500, blank=False)
-    qualifications = models.TextField(max_length=20000, blank=False)
-    link = models.CharField(max_length=75, blank=False)
+    title = models.CharField(max_length=150, blank=False)
+    location = models.CharField(max_length=50, blank=False)
+    function = models.CharField(max_length=100, blank=False)
+    industry = models.CharField(max_length=100, blank=False)
+    description = models.TextField(max_length=1000, blank=False)
+    qualifications = models.TextField(max_length=50000, blank=False)
+    scrap_date = models.DateField(auto_now_add=True)
+    hyperlink = models.CharField(max_length=150, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
